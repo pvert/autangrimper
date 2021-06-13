ca25bb46119bf84cbb1913b5e5a818c86c0f6601
Pierre Vert <pierr65@gmail.com>
2021-06-13 23:54:14 +0200

    first commit

--- autangrimper.php
+++ autangrimper.php
@@ -7,7 +7,7 @@
  * Author URI:      https://github.com/pvert/
  * Text Domain:     atgp
  * Domain Path:     /languages
- * Version:         0.1.1
+ * Version:         0.1.0
  * @package         atgp
  */
 
