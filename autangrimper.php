a73153b58ccc84988605a99bab45a4e1d629258a
Pierre Vert <pierr65@gmail.com>
2021-08-17 12:22:01 +0200

    release 1.0.0

--- autangrimper.php
+++ autangrimper.php
@@ -7,7 +7,7 @@
  * Author URI:      https://github.com/pvert/
  * Text Domain:     atgp
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         1.0.0
  * @package         atgp
  */
 
