8828d6560027f5712ad2de70addedcd3ebc452cc
Pierre Vert <pierr65@gmail.com>
2022-06-23 00:35:27 +0200

    ajout saison sur pdf

--- autangrimper.php
+++ autangrimper.php
@@ -7,7 +7,7 @@
  * Author URI:      https://github.com/pvert/
  * Text Domain:     atgp
  * Domain Path:     /languages
- * Version:         1.2.9
+ * Version:         1.3.0
  * @package         atgp
  */
 
